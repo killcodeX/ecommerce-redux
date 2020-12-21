ba4e42ce51e266f1aa7f52fba390346323ecdf2b
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-12-21 13:20:42 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Header from './components/layout/header';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div className="App"> 
       <button className='btn btn-primary'>HI</button>
     </div>
   );
