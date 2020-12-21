8a171219a036d14292cacc90f53b05b7bf9eadcf
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-12-21 12:57:47 +0530

    initial changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from './components/layout/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <button className='btn btn-primary'>HI</button>
+    </div>
+  );
+}
+
+export default App;
