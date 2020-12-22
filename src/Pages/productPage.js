90949904e943544c448a4021255cb3a4b3e011b7
Aaquib Ahmed <aaquibskimbox@gmail.com>
2020-12-22 15:03:42 +0530

    product page done

--- src/Pages/productPage.js
+++ src/Pages/productPage.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import Data from '../mockSingleProduct';
+import React from "react";
+import { Link } from "react-router-dom";
+import Data from "../mockSingleProduct";
+import { AddToCart } from "../components";
 
 export default function ProductPage() {
-    return (
-        <div className='section'>
-            <div className='container'>Hi</div>
+  return (
+    <div className="section product-page mt-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-6 brdr">
+            <img className="product-image" src={Data.image} alt={Data.tile} />
+          </div>
+          <div className="col-6 pt-5">
+            <h2 className="pb-4">{Data.title}</h2>
+            <p>
+              <strong>Category : </strong>
+              <span className="text-muted">{Data.category}</span>
+            </p>
+            <p>
+              <strong>Description : </strong>
+              <span className="text-muted">{Data.description}</span>
+            </p>
+            <p>
+              <strong>Price : </strong>
+              {"$" + Data.price}
+            </p>
+            <div className="card-down">
+              <AddToCart id={Data.id} />
+              <Link to="/" type="button" className="btn-buy" style={{textDecoration:'none'}}>
+                Add To Cart
+              </Link>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/products.js
+++ src/components/products.js
@@ -1,45 +1,25 @@
-import React, { useState } from "react";
-import {AddToCart} from '../components';
-import { Collapse, Button } from "react-bootstrap";
-
+import React from "react";
+import {Link} from 'react-router-dom';
 export default function Products(props) {
-
-  const {data} = props;
-  const [open, setOpen] = useState(false);
+  const { data } = props;
 
   return (
     <div className="col-sm-12 col-lg-3 mb-4">
-      <div className="card shadow">
-        <div className="card-head ">
-          <img src={data.image} alt={data.title} />
-          <div className="card-detail pt-4">
-            <p className='font-weight-bold'>{data.title}</p>
-            <Button
-                className='coll-btn'
-              onClick={() => setOpen(!open)}
-              aria-controls="example-collapse-text"
-              aria-expanded={open}
-            >
-              {open? 'Read Less':'Read Description'}
-            </Button>
-            <Collpase data={data.description} open={open} />
+      <Link to="/product" style={{ textDecoration: 'none' }}>
+        <div className="card shadow">
+          <div className="card-head ">
+            <img src={data.image} alt={data.title} />
+            <div className="card-detail pt-4">
+              <p className="font-weight-bold">{data.title}</p>
+              <p>
+              <strong>Category : </strong>
+              <span className="text-muted">{data.category}</span>
+            </p>
+            <p className="mt-3 font-weight-bold">{"$" + data.price}</p>
+            </div>
           </div>
         </div>
-        <div className='card-down'>
-            <AddToCart id={data.id}/>
-            <p className='mt-3 font-weight-bold'>{'$' + data.price}</p>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
-
-const Collpase = (props) => {
-  return (
-    <Collapse in={props.open}>
-      <div id="example-collapse-text" className='text-muted mt-3'>{props.data}</div>
-    </Collapse>
-  );
-};
-
-
