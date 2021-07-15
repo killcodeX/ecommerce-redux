c816a5ba5811a5a66fcb92c9ef90a77b93c3377f
Aaquib Ahmed <aaquibskimbox@gmail.com>
2021-07-15 21:56:24 +0530

    all product added to database

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -11,7 +11,13 @@ export const displaySingleProduct = async (req, res) =>{
 
 export const addProduct = async (req, res) => {
   const body = req.body;
-
   console.log('data got in backend',body)
+  const newProduct = new ProductMessage(body);
+  try {
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 
 }
